1ab6e9233c53f243f3524caddfebec61b483698f
Mishel <minatosavich@gmail.com>
2021-12-21 13:03:49 +0300

    feat: index.html add Calculator route

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '/bundle.js',
+    filename: 'bundle.js',
     publicPath: './',
   },
   plugins: [
