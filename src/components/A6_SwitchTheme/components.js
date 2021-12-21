0f6efee0ebfd82536b3bc4d2f5f5312a1996fcfa
Mishel <minatosavich@gmail.com>
2021-12-21 13:08:34 +0300

    feat: index.html add Calculator route

--- src/components/A6_SwitchTheme/components.js
+++ src/components/A6_SwitchTheme/components.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 export const Select = styled.select`
   margin: 10px 0px 50px;
-  width: 400px;
+  width: 250px;
   font-size: 32px;
   padding: 23px;
   border-radius: 8px;
@@ -11,4 +11,6 @@ export const Select = styled.select`
   border: 2px solid #434343;
   background-color: ${({ theme }) => theme.colors.backgroundButton};
   color: ${({ theme }) => theme.colors.main};
+
+  ${({ theme }) => theme.above.xs({ width: '400px', fontSize: theme.fontSizes[1] })};
 `
