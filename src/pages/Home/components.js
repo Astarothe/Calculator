d3cb934f3f069aa6ccdcdf974b692bad9bb9652e
Mishel <minatosavich@gmail.com>
2021-12-29 20:55:01 +0300

    fix: remove null in styled-component and refactor

--- src/pages/Home/components.js
+++ src/pages/Home/components.js
@@ -1,30 +1,35 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
 export const Wrapper = styled.div`
-  display: ${props => props.display || 'null'};
-  justify-content: ${props => props.justify || null};
-  flex-direction: ${props => props.direction || null};
-  flex-wrap: ${props => props.wrap || null};
-  width: ${props => props.width || null};
-  height: ${props => props.height || null};
-  margin: ${props => props.margin || null};
-  padding: ${props => props.padding || null};
-  max-width: ${props => props.maxWidth || null};
+
+  ${props => css`
+    display: ${props.display};
+    justify-content: ${props.justify };
+    flex-direction: ${props.direction};
+    width: ${props.width};
+    height: ${props.height};
+    margin: ${props.margin };
+    padding: ${props.padding };
+    max-width: ${props.maxWidth};
+    flex-wrap: ${props.wrap };
+  `}
 `
 
 export const MainWrapper = styled.main`
   transform: translate(-100%);
-  flex-direction: column;
-  display: ${props => props.display || 'block'};
-  justify-content: ${props => props.justify || null};
-  flex-direction: ${props => props.direction || null};
-  flex-wrap: ${props => props.wrap || null};
-  width: ${props => props.width || null};
-  height: ${props => props.height || null};
-  margin: ${props => props.margin || null};
-  padding: ${props => props.padding || null};
-  border-left: ${props => props.borderLeft || null};
-  max-width: ${props => props.maxWidth || null};
+
+  ${props => css`
+    display: ${props.display};
+    justify-content: ${props.justify };
+    flex-direction: ${ props.direction || "column"};
+    flex-wrap: ${props.wrap};
+    width: ${ props.width };
+    height: ${ props.height };
+    margin: ${ props.margin };
+    padding: ${ props.padding };
+    border-left: ${ props.borderLeft };
+    max-width: ${ props.maxWidth };
+  `}
 
   ${({theme}) => theme.above.large({flexDirection: "row"})}
   `
